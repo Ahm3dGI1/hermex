6a9551100327677a7e02bce832d3e1ad7b5b1222
ShinnosukeUesaka <45286939+ShinnosukeUesaka@users.noreply.github.com>
2025-04-13 07:58:51 +0900

    yapping and timeout

--- frontend/src/components/Yap.tsx
+++ frontend/src/components/Yap.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 
-export default function Yap({ isDown }: { isDown: boolean }): React.ReactElement {
+export default function Yap({ isDown, isAnimating }: { isDown: boolean, isAnimating: boolean }): React.ReactElement {
   const [currentImage, setCurrentImage] = useState(1);
-  const [isAnimating, setIsAnimating] = useState(false);
 
   useEffect(() => {
     let interval: number | null = null;
@@ -20,15 +19,10 @@ export default function Yap({ isDown }: { isDown: boolean }): React.ReactElement
     };
   }, [!isDown, isAnimating]);
 
-  const handleClick = () => {
-    setIsAnimating(prev => !prev);
-  };
-
   return (
     <div className={`absolute top-107 transition-all duration-500 ease-in-out ${!isDown ? '-left-10' : '-left-[600px]'
       } justify-center`}>
       <button
-        onClick={handleClick}
         className="flex items-center justify-center"
       >
         <img
