487ceb11ff2ed513f13ba8ca78bcb48d295ff1b2
Ahmed Ibrahim <ahmedgouda797@gmail.com>
2025-04-12 12:57:36 +0900

    Api connected and frontend sending requests

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,33 @@
+from typing import Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import Body
 from fastapi.middleware.cors import CORSMiddleware
+import uuid
 
 app = FastAPI()
 
-# Allow frontend to communicate
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hermex-gamma.vercel.app/", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "https://hermex-gamma.vercel.app/"],  # frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router()
+class PreprocessRequest(BaseModel):
+    youtube_link: str
+
+@app.get("/api")
+
+
+
+@app.post("/api/preprocess")
+def preprocess_video(data: PreprocessRequest):
+    session_id = str(uuid.uuid4())
+    return {
+        "status": "started",
+        "session_id": session_id,
+        "message": f"Started processing {data.youtube_link}"
+    }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,54 @@
+import { useState } from 'react';
+
 import './App.css'
 
-import InputField from './components/input_field'
 
 function App() {
+  const [youtubeLink, setYoutubeLink] = useState<string>('');
+
+  // Function to handle input submission
+  const handleInput = async (youtubeLink: string) => {
+    console.log("Input received:", youtubeLink);
+
+    const response = await fetch('http://127.0.0.1:8000/api/preprocess', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ youtube_link: youtubeLink }),
+    });
+
+    const data = await response.json();
+    console.log("Preprocessing response:", data);
+  }
+
+  // Press Enter to submit the input
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      const inputValue = e.currentTarget.value.trim();
+      if (inputValue) {
+        handleInput(inputValue);
+      }
+    }
+  };
+
+
   return (
     <>
       <div className='main flex justify-center items-center h-screen bg-blue-200'>
-        <InputField />
+        <div className="input flex justify-around items-center w-4/5">
+          <input
+            className="link-field w-4/5 rounded bg-amber-50 select-none"
+            type="text"
+            placeholder="Enter a video link to get started..."
+            value={youtubeLink}
+            onChange={(e) => setYoutubeLink(e.target.value)}
+            onKeyPress={handleKeyPress}
+          />
+          <button className="submit-button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleInput(youtubeLink)}>
+            Start Class
+          </button>
+        </div>
       </div>
     </>
   )
