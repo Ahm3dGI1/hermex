c3be17229f6b62287a13d56baf0764a4a68b3f0c
Ahmed Ibrahim <ahmedgouda797@gmail.com>
2025-04-12 11:14:43 +0900

    input field

---
+++ frontend/src/components/input_field.tsx
@@ -0,0 +1,11 @@
+import "../index.css";
+
+function InputField() {
+    return (
+        <div className="input-field">
+            <input className="w-3/5" type="text" placeholder="Enter a video link to get satarted..." />
+        </div>
+    );
+}
+
+export default InputField;
