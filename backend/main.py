b2f98e41c3e38f2a7afb04896afaa691e9592420
Ahmed Ibrahim <ahmedgouda797@gmail.com>
2025-04-12 11:28:37 +0900

    fastapi init

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import preprocess
+
+app = FastAPI()
+
+# Allow frontend to communicate
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://hermex-gamma.vercel.app/", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(preprocess.router, prefix="/api/preprocess")
